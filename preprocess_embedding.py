d1c6a7a252de80f73fa5abe70abd7c35a0e4735f
dankmachine <williamjow67@gmail.com>
2019-05-06 14:14:46 -0700

    minor progress

--- preprocess_embedding.py
+++ preprocess_embedding.py
@@ -2,6 +2,7 @@ import sys, pickle, re, string
 import numpy as np
 import pandas as pd
 from collections import Counter
+from keras.preprocessing.text import Tokenizer
 
 # text processing stuff
 punc_regex = re.compile("[{}]".format(re.escape(string.punctuation)))
@@ -11,9 +12,11 @@ with open('vocab.p', 'rb') as vocab_pickle:
     vocab = pickle.load(vocab_pickle)
 vocab_set = set(vocab.keys())
 
-loans = pd.read_csv("data/kiva_loans.csv")
-uses = loans.loc[:, "use"].dropna()
-tags = loans.loc[:, "tags"].dropna()
+# data
+with open('X_train.p', 'rb') as X_train_pickle:
+    X_train = pickle.load(X_train_pickle)
+uses = X_train.loc[:, "use"]
+tags = X_train.loc[:, "tags"]
 
 # progress tracking
 processed = 0
--- preprocess_final.py
+++ preprocess_final.py
@@ -3,5 +3,8 @@ import pandas as pd
 loans = pd.read_csv("data/kiva_loans.csv")
 
 # columns that are prior knowledge
+# - use and tags are seemingly arbitrary so we use the word embeddings for those
+# - activity and sector seem to be restricted to a reasonable set of values,
+#   so we can just one-hot those
 loans = loans.loc[:, ["id", "loan_amount", "activity", "sector", "use",
                       "country_code", "tags", "borrower_genders"]]
